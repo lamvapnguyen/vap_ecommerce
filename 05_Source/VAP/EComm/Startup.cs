05c316264b1c3fc90a234a89a8e0a46653185f42
ntlam <ntlam@indivisys.jp>
2017-12-21 10:52:12 +0700

    tao solution code
    tạo mới solution
    + mvc5: VAP.EComm
    + entity framework 6.1.3

---
+++ 05_Source/VAP/EComm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EComm.Startup))]
+namespace EComm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
