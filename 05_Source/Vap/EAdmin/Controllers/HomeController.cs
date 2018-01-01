36bc86966b8769f3a9e95fb3c631e24a5e5fd356
lamthiduyen <lamthiduyen@gmail.com>
2018-01-01 10:39:47 +0700

    Tạo giao diện trang admin (top, left menu). Thay doi version bootstrap, jquery

--- 05_Source/Vap/EAdmin/Controllers/HomeController.cs
+++ 05_Source/Vap/EAdmin/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading;
+using System.Globalization;
 
 namespace EAdmin.Controllers
 {
     public class HomeController : Controller
     {
+        public CultureInfo CultrureInfo { get; private set; }
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +30,18 @@ namespace EAdmin.Controllers
 
             return View();
         }
+        public ActionResult ChangeLang(string lang)
+        {
+            if(lang!=null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = lang;
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
+        }
+
     }
 }
